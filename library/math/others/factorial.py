0086f2cc26596cf3afaa0c4aaf7f6cb372f723e0
kenny <tyanyuki3.39@gmail.com>
2023-09-06 21:56:03 +0900

    Update math library - factorial, n choose k

--- library/math/others/factorial.py
+++ library/math/others/factorial.py
@@ -1,10 +1,22 @@
+import numpy as np
+import sympy as sp
+
 # 階乗
 # 計算量：~O(N)
 
-N = int(input())
-MOD = 10**9+7
-fact = [1]*(N+1)
-for i in range(1, N+1):
-    fact[i] = i * fact[i-1] % MOD
 
-# print(fact[N])
+def factorial(N):
+    # MOD = 10**9 + 7
+    fact = [1] * (N + 1)
+    for i in range(1, N + 1):
+        # fact[i] = i * fact[i - 1] % MOD
+        fact[i] = i * fact[i - 1]
+    return fact[N]
+
+
+# 自作, math, numpy, sympyの速度はほぼ変わらない
+x = factorial(10000)
+
+x = np.math.factorial(10000)
+
+x = sp.factorial(10000)
--- library/math/others/nChooseK.py
+++ library/math/others/nChooseK.py
@@ -1,17 +1,39 @@
+import math
+import time
+
+import sympy as sp
+
 # 二項係数 nCk
 # パスカルの三角形を用いる
 # 計算量：~O(N)
 
-N = int(input())
-C = []
-for i in range(N+1):
-    C.append([0]*(N+1))
-C[0][0] = 1
-MOD = 10**9+7
-for i in range(1, N+1):
-    C[i][0] = 1
-    for j in range(1, i+1):
-        C[i][j] = C[i-1][j-1] + C[i-1][j]
 
-# C[n][k]
-# print(C[3][2])
+def n_choose_k(N):
+    C = []
+    for i in range(N + 1):
+        C.append([0] * (N + 1))
+    C[0][0] = 1
+    MOD = 10**9 + 7
+    for i in range(1, N + 1):
+        C[i][0] = 1
+        for j in range(1, i + 1):
+            C[i][j] = C[i - 1][j - 1] + C[i - 1][j]
+            C[i][j] %= MOD
+    return C
+
+
+# 自作が一番遅い, math, sympyの速度はほぼ変わらない
+time_start = time.time()
+C = n_choose_k(1000)
+time_end = time.time()
+print(f"Time: {time_end - time_start:.3f} sec")
+
+time_start = time.time()
+x = math.comb(100000, 7777)
+time_end = time.time()
+print(f"Time: {time_end - time_start:.3f} sec")
+
+time_start = time.time()
+x = sp.binomial(100000, 7777)
+time_end = time.time()
+print(f"Time: {time_end - time_start:.3f} sec")
