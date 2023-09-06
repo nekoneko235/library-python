fe48d79b90378967902140c5279caaaae6a798cc
kenny <tyanyuki3.39@gmail.com>
2023-09-06 21:13:31 +0900

    Update math library

--- library/math/primality_check.py
+++ library/math/primality_check.py
@@ -1,11 +1,19 @@
+import sympy as sp
+
 # 素数判定
 # 計算量：~O(sqrt(N))
+
 
 def is_prime(N):
     if N == 1:
         return False
-    LIMIT = int(N ** 0.5)
+    LIMIT = int(N**0.5)
     for i in range(2, LIMIT + 1):
         if N % i == 0:
             return False
     return True
+
+
+# SymPyの素数判定(自作ライブラリより高速)
+x = sp.isprime(67280421310721)
+x = sp.isprime(170141183460469231731687303715884105727)
