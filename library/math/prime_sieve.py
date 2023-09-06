437be2022e7d6753b7f878d64fa33191f48c2530
kenny <tyanyuki3.39@gmail.com>
2023-09-07 08:23:59 +0900

    Update sieve of eratosthenes by using numpy and sympy

--- library/math/prime_sieve.py
+++ library/math/prime_sieve.py
@@ -1,12 +1,18 @@
+import time
+
+import numpy as np
+import sympy as sp
+
 # エラトステネスの篩
 # 計算量：~O(N log(log(N)))
+
 
 def sieve_of_Eratosthenes(N):
     primes = [True] * (N + 1)
     res = []
 
     # エラトステネスの篩
-    LIMIT = int(N ** 0.5)
+    LIMIT = int(N**0.5)
     for i in range(2, LIMIT + 1):
         if primes[i] is True:
             # x = 2i, 3i, 4i, ...と, N 以下の間ループし続ける
@@ -21,5 +27,36 @@ def sieve_of_Eratosthenes(N):
     return res
 
 
-arr = sieve_of_Eratosthenes(100)
-print(arr)
+def sieve_of_Eratosthenes_numpy(N):
+    primes = np.ones(N + 1, dtype=bool)
+    primes[:2] = False  # 0, 1 は素数ではない
+
+    LIMIT = int(N**0.5) + 1
+
+    for i in range(2, LIMIT):
+        if primes[i]:
+            primes[i * 2 :: i] = False
+
+    return np.nonzero(primes)[0]
+
+
+# NumPy >> Pure Python >> SymPy の順に高速
+time_start = time.time()
+# arr = sieve_of_Eratosthenes(10**8)
+# print(arr)
+time_end = time.time()
+print(f"Time: {time_end - time_start:.3f} sec")
+
+# Numpy
+time_start = time.time()
+arr = sieve_of_Eratosthenes_numpy(10**8)
+# print(arr)
+time_end = time.time()
+print(f"Time: {time_end - time_start:.3f} sec")
+
+# SymPy
+time_start = time.time()
+# primes = list(sp.primerange(10**6))
+# print(primes)
+time_end = time.time()
+print(f"Time: {time_end - time_start:.3f} sec")
