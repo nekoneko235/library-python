7f3ee1c6ee27a51ed662f4ddd4f56e0bd8dc68f3
kenny <tyanyuki3.39@gmail.com>
2023-09-07 21:52:42 +0900

    Refactoring fast_exponentiation.py and rename enum_divisors

--- library/others/fast_exponentiation.py
+++ library/others/fast_exponentiation.py
@@ -1,8 +1,16 @@
+import time
+
 # 繰り返し二乗法
-# Pythonには「a^NをMで割った余り」を計算できる関数pow()が標準で存在する
+# Pythonには「N^XをMで割った余り」を計算できる関数pow()が標準で存在する
 # 計算量：O(logN)
 
-N = int(input())
-MOD = 10**9+7
-ans = pow(3, N, MOD)
+N = 2
+X = 10
+MOD = 10**9 + 7
+
+# Pure Python で十分高速
+time_start = time.time()
+ans = pow(N, X, MOD)
 print(ans)
+time_end = time.time()
+print(f"Time: {time_end - time_start:.3f} sec")
