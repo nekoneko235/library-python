3c2d3a51663a4f1f297677768ef09710b1b4a970
kenny <tyanyuki3.39@gmail.com>
2023-09-07 21:39:40 +0900

    Update prime_factorization.py

--- library/math/prime_factorization.py
+++ library/math/prime_factorization.py
@@ -1,14 +1,38 @@
+import time
+
+import numpy as np
+import sympy as sp
+
 # 素因数分解
 # 計算量：~O(sqrt(N))
 
+
 def prime_factorization(N):
     primes = []
-    LIMIT = int(N ** 0.5)
+    LIMIT = int(N**0.5)
     for i in range(2, LIMIT + 1):
-        while N % i == 0:
-            N /= i
-            primes.append(i)
-    # N が素数の場合
+        if N % i == 0:
+            ex = 0
+            while N % i == 0:
+                ex += 1
+                N //= i
+            primes.append((i, ex))
     if N >= 2:
-        primes.append(int(N))
+        primes.append((N, 1))
     return primes
+
+
+# SymPy >> Pure Python の順に高速
+time_start = time.time()
+primes = prime_factorization(10**16 + 781222)
+print(primes)
+time_end = time.time()
+print(f"Time: {time_end - time_start:.3f} sec")
+
+
+# SymPy
+time_start = time.time()
+primes = sp.factorint(10**16 + 781222)
+print(primes)
+time_end = time.time()
+print(f"Time: {time_end - time_start:.3f} sec")
